4f80b4b14c2ed1e42e110adb70273644a947cf15
syahshiyahr <syahshiyahr@gmail.com>
2021-06-05 01:29:20 +0800

    bind user info in home fragment

--- app/src/main/java/com/bearbrand/fourney/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bearbrand/fourney/ui/home/HomeFragment.kt
@@ -3,19 +3,22 @@ package com.bearbrand.fourney.ui.home
 import android.content.Intent
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.bearbrand.fourney.R
 import com.bearbrand.fourney.activity.AuthActivity
 import com.bearbrand.fourney.adapter.AdvertiseAdapter
 import com.bearbrand.fourney.databinding.FragmentHomeBinding
 import com.bearbrand.fourney.helper.OnItemClickListener
 import com.bearbrand.fourney.model.Place
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -24,6 +27,9 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private var adapter: AdvertiseAdapter? = null
     private lateinit var reference: CollectionReference
+    private lateinit var data: DocumentReference
+    private val firestore = FirebaseFirestore.getInstance()
+    var name: String = ""
 
 
     override fun onCreateView(
@@ -32,8 +38,9 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+
+        checkUser()
         //init color text
-        var name = "Pengunjung"
         val styledText = "Halo, <font color='#2D74FF'>$name</font>"
         binding.tvHelloName.setText(Html.fromHtml(styledText), TextView.BufferType.SPANNABLE)
 
@@ -47,6 +54,27 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         loadData()
+    }
+
+    private fun checkUser() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null){
+            val uid = user?.uid!!
+            data = firestore.collection("users").document(uid)
+            data.get().addOnSuccessListener {
+                name = it.getString("name")!!
+
+                //init name because no effect when it called in onCreateView
+                val styledText = "Halo, <font color='#2D74FF'>$name</font>"
+                binding.tvHelloName.setText(Html.fromHtml(styledText), TextView.BufferType.SPANNABLE)
+
+                binding.tvLogin.visibility = View.GONE
+            }.addOnFailureListener {
+                Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            name = "Pengunjung"
+        }
     }
 
     private fun loadData() {
