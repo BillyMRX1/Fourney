e59f22373398ff384e51e18aeb033a38dbc905ca
BillyMRX1 <brilianadeputra@gmail.com>
2021-06-05 20:09:43 +0800

    Add Challenge

---
+++ app/src/main/java/com/bearbrand/fourney/activity/ChallengeActivity.kt
@@ -0,0 +1,26 @@
+package com.bearbrand.fourney.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bearbrand.fourney.R
+import com.bearbrand.fourney.databinding.ActivityChallengeBinding
+import com.bearbrand.fourney.ui.challenge.ChallengeFragment
+
+class ChallengeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChallengeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChallengeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mFragmentManager = supportFragmentManager
+        val mFragmentChallenge = ChallengeFragment()
+
+        mFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, mFragmentChallenge, ChallengeFragment::class.java.simpleName)
+            commit()
+        }
+    }
+}
