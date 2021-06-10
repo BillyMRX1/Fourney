53100bb30543e49d23cb7ea8c58c2dbd8dcdf0ab
BillyMRX1 <brilianadeputra@gmail.com>
2021-06-10 19:37:45 +0800

    Tensor Flow Lite and Challenge Implementation Fixed

--- app/src/main/java/com/bearbrand/fourney/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/bearbrand/fourney/ui/camera/CameraActivity.kt
@@ -1,12 +1,15 @@
 package com.bearbrand.fourney.ui.camera
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bearbrand.fourney.databinding.ActivityCameraBinding
 import com.bearbrand.fourney.tflite.Classifier
+import com.bearbrand.fourney.tflite.IClassifier
+import com.google.firebase.firestore.FirebaseFirestore
 import com.wonderkiln.camerakit.*
 import java.util.concurrent.Executors
 
@@ -15,11 +18,17 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private lateinit var classifier: Classifier
     private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var objectId: String
+    private lateinit var locationId: String
+    private lateinit var result: List<IClassifier.Recognition>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        objectId = intent.getStringExtra(OBJECT_ID).toString()
+        locationId = intent.getStringExtra(LOCATION_ID).toString()
+        Log.d("OBJECT: ", objectId)
         binding.cameraView.start()
         binding.cameraView.addCameraKitListener(object : CameraKitEventListener {
             override fun onEvent(p0: CameraKitEvent?) {}
@@ -29,9 +38,22 @@ class CameraActivity : AppCompatActivity() {
             override fun onImage(p0: CameraKitImage) {
                 var bitmap = p0.bitmap
                 bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false)
-                val result = classifier.recognizeImage(bitmap)
-                binding.viewSuccess.tvAnswer.text = result[0].title
-                Toast.makeText(applicationContext, result[1].title, Toast.LENGTH_SHORT).show()
+                result = classifier.recognizeImage(bitmap)
+                Log.d("SCAN: ", result.toString())
+                if (result[0].confidence >= 0.9) {
+                    Log.d("ID", "title "+result[0].title)
+                    Log.d("ID", "objectId $objectId")
+                    if (result[0].title == objectId) {
+                        getData()
+                        binding.viewSuccess.root.visibility = View.VISIBLE
+                    } else {
+                        binding.viewFailed.root.visibility = View.VISIBLE
+                        Log.d("Failed: ","Failed 1")
+                    }
+                } else {
+                    binding.viewFailed.root.visibility = View.VISIBLE
+                    Log.d("Failed: ","Failed 3")
+                }
             }
 
             override fun onVideo(p0: CameraKitVideo?) {}
@@ -39,7 +61,6 @@ class CameraActivity : AppCompatActivity() {
 
         binding.btnShutter.setOnClickListener {
             binding.cameraView.captureImage()
-            binding.viewSuccess.root.visibility = View.VISIBLE
             binding.cameraView.stop()
         }
         initTensorFlowAndLoadModel()
@@ -53,6 +74,21 @@ class CameraActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         executor.execute { classifier.close() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getData() {
+        val reference = FirebaseFirestore.getInstance().collection("objects").document(locationId)
+            .collection("listObjects").document(objectId)
+        reference.addSnapshotListener { data, _ ->
+            if (data != null) {
+                binding.apply {
+                    viewSuccess.tvCoin.text = "+ " + data.getLong("coint").toString() + " CP"
+                    viewSuccess.tvXp.text = "+ " + data.getLong("xp").toString() + " XP"
+                    viewSuccess.tvAnswer.text = data.getString("desc")
+                }
+            }
+        }
     }
 
     private fun initTensorFlowAndLoadModel() {
@@ -74,5 +110,7 @@ class CameraActivity : AppCompatActivity() {
         private const val MODEL_PATH = "mobilenet_quant_v1_224.tflite"
         private const val LABEL_PATH = "labels.txt"
         private const val INPUT_SIZE = 224
+        const val OBJECT_ID = "object_id"
+        const val LOCATION_ID = "location_id"
     }
 }
